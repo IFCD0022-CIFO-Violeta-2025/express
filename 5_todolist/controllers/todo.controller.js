e59687c87e869d017ff10763cd964845e5b6e63d
marc <147423007+marcaixs@users.noreply.github.com>
2025-10-08 18:59:23 +0200

    fix: change status code of updateTodo from 200 to 201

--- 5_todolist/controllers/todo.controller.js
+++ 5_todolist/controllers/todo.controller.js
@@ -100,7 +100,7 @@ function updateTodo(req, res){
     try {
          
         const todo = todoModel.update(req.params.id, req.body);
-        res.status(200).json({
+        res.status(201).json({
             success: true,
             message: "Tarea actualizada",
             data: todo
